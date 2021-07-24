c652629200e6ad5ee7062ded417b382e1e2f0211
e1835104 <87562735+e1835104@users.noreply.github.com>
2021-07-24 11:41:09 +0900

    最終課題

---
+++ Finalkadai.js
@@ -0,0 +1,50 @@
+function English(language) {
+    var message = "";
+
+    if (language == 1) {
+        message = "不正解";
+    } else if (language == 2) {
+        message = "不正解";
+    } else if (language == 3) {
+        message = "正解";
+    } else  {
+        message = "想像されていない引数です。";
+    }
+
+    alert(message);
+
+}
+
+function France(language) {
+    var message = "";
+
+    if (language == 1) {
+        message = "正解";
+    } else if (language == 2) {
+        message = "不正解";
+    } else if (language == 3) {
+        message = "不正解";
+    } else  {
+        message = "想像されていない引数です。";
+    }
+
+    alert(message);
+
+}
+
+function Indonesia(language) {
+    var message = "";
+
+    if (language == 1) {
+        message = "不正解";
+    } else if (language == 2) {
+        message = "正解";
+    } else if (language == 3) {
+        message = "不正解";
+    } else  {
+        message = "想像されていない引数です。";
+    }
+
+    alert(message);
+
+}
